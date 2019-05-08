e62c78d62d759ef332eb70ea7bed2d6e3bf2ebfa
shubham kumar <shubham.chumber@smu.ca>
2019-05-08 02:55:34 -0300

    Reverse of a string ( Palindrome )
    Using normal String and a for loop

--- JavaAlgorithms/src/src/algo/string/ReverseOfString.java
+++ JavaAlgorithms/src/src/algo/string/ReverseOfString.java
@@ -1,10 +1,24 @@
 package src.algo.string;
 
+import java.util.Scanner;
+
 public class ReverseOfString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		
+		String input = scanner.nextLine();
+		String reverse = "";
+		
+		for (int i =input.length()-1;i>=0;i--) {
+			reverse = reverse+ input.charAt(i);
+		}
+		if (input.toUpperCase().equals(reverse.toUpperCase())) {
+			System.out.println("Its a palidrome !! :) ");
+		}
+		else 
+			System.out.println("Oopsie!! Its not a palindrome");
 	}
 
 }
