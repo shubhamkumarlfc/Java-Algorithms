188f4448c4dcde9ef1d06a0870b2bda21d13fb8c
shubham kumar <shubham.chumber@smu.ca>
2019-05-08 02:58:59 -0300

    Reverse of a string using append of String builder

--- JavaAlgorithms/src/src/algo/string/ReverseOfString2.java
+++ JavaAlgorithms/src/src/algo/string/ReverseOfString2.java
@@ -1,20 +1,19 @@
 package src.algo.string;
-
 import java.util.Scanner;
 
-public class ReverseOfString {
+public class ReverseOfString2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
 		
 		String input = scanner.nextLine();
-		String reverse = "";
+		StringBuilder reverse = new StringBuilder();
 		
 		for (int i =input.length()-1;i>=0;i--) {
-			reverse = reverse+ input.charAt(i);
+			reverse = reverse.append(input.charAt(i));
 		}
-		if (input.toUpperCase().equals(reverse.toUpperCase())) {
+		if (input.toUpperCase().equals(reverse.toString().toUpperCase())) {
 			System.out.println("Its a palidrome !! :) ");
 		}
 		else 
